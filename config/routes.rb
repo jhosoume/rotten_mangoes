a023646a657f4c2d89f0ac7c3362fca1de37772d
Juliana Hosoume <ju.hosoume@gmail.com>
2016-06-22 20:48:25 -0700

    change resources to resource for session

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ RottenMangoes::Application.routes.draw do
     resources :reviews, only: [:new, :create]
   end
 
-  resources :sessions, only: [:new, :create, :destroy]
+  resource :session, only: [:new, :create, :destroy]
 
   root to: 'movies#index'
 
